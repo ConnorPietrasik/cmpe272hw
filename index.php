f7194694cf1d0d20687cd304217c83f0322b4535
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-10-10 16:59:59 -0700

    honestly not much, switching to laptop though

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home - Stuff Company</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <?php readfile("nav.html"); ?>
+    </body>
+</html>
