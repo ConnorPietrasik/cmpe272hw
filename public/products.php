3dc6b34df17583a306bd5b203890d64f10c036a2
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-10-11 04:51:03 -0700

    added products

---
+++ public/products.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home - StuffCo</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <?php readfile("nav.html"); ?>
+        <h2>Products:</h2>
+        <p class="disclaimer">Disclaimer: All purchases are final, no refunds under any circumstances.</p>
+        <?php  
+            $products = scandir('products');
+            foreach($products as $prod){
+                if ($prod !== "." && $prod !== ".."){
+                    echo "<hr><h3 class=\"prod-name\">$prod</h3>";
+                    echo "<img src=\"products/$prod/main.png\" alt=\"$prod image\">";
+                    echo "<p class=\"prod-desc\">";
+                    readfile("products/$prod/desc.txt");
+                    echo "</p>";
+                    echo "<p class=\"prod-price\">";
+                    readfile("products/$prod/price.txt");  
+                    echo "</p>";
+                }
+            }
+        ?>
+    </body>
+</html>
