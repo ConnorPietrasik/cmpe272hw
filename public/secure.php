ebceb883b634b3b4a7cb89793da7642a5f3100a7
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-10-11 18:20:38 -0700

    added secure test

---
+++ public/secure.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Secure - StuffCo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <?php readfile("nav.html"); ?>
+    <h2>Users:</h2>
+    <?php readfile("../users.txt"); ?>
+</body>
+
+</html>
