39774a2b0c675fd395aa42e39f2c3c0a96afb2a5
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-10-18 18:30:57 -0700

    using repo for midterm

---
+++ public/midterm/globals.php
@@ -0,0 +1,30 @@
+<!-- Program to display environment variables -->
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<body>
+    <table border="0" cellpadding="2" cellspacing="0" width="100%">
+
+        <?php
+
+        // Add code HERE
+        echo $GLOBALS;
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ?>
+
+    </table>
+</body>
+
+</html>
