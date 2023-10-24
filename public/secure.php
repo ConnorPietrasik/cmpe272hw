ac50261b0d03e559ef9369cbbd886f60d824821b
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-10-23 23:19:02 -0700

    added bad login system as assignment says

--- public/secure.php
+++ public/secure.php
@@ -9,8 +9,36 @@
 
 <body>
     <?php readfile("nav.html"); ?>
-    <h2>Users:</h2>
-    <?php readfile("../users.txt"); ?>
+
+    <?php 
+        function login() {
+            if (!$_POST["username"] || !$_POST["password"]){
+                readfile("login.html");
+            }
+            else {
+                $file = fopen("../logins.txt", "r");
+                if (!$file){
+                    echo "ERROR READING LOGIN FILE";
+                }
+                else{
+                    while (($line = fgets($file)) !== false){
+                        $tmp = explode(",", $line);
+                        if ($tmp[0] == $_POST["username"] && $tmp[1] == $_POST["password"]){
+                            echo "<h2>Users:</h2>";
+                            readfile("../users.txt");
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+
+        if (!login()){
+            echo "<h2 class=\"error\">Wrong user/pass, try again:</h2>";
+            readfile("login.html");
+        }
+    ?>
 </body>
 
 </html>
