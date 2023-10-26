3e1b036f8cba0d5859419468b747dd1abb31efa7
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-10-25 20:08:05 -0700

    updated test

--- public/product_page.php
+++ public/product_page.php
@@ -17,7 +17,7 @@
 <body>
     <?php readfile("nav.html"); ?>
     <h2>Product:</h2>
-    <pre> <?php var_dump(array_keys($_POST)) ?> </pre>
+    <pre> <?php echo array_keys($_POST)[0] ?> </pre>
 </body>
 
 </html>
