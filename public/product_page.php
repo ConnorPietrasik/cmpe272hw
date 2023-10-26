a0e170f6b479974df642502b9d02e49b81ea072a
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-10-25 20:01:19 -0700

    somewhat added individual product pages

---
+++ public/product_page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<?php 
+    if (empty($_POST)){
+        echo "Bad link! Go away!";
+        die();
+    }
+?>
+
+<head>
+    <title>Products - StuffCo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <?php readfile("nav.html"); ?>
+    <h2>Product:</h2>
+    <pre> <?php var_dump($_POST) ?> </pre>
+</body>
+
+</html>
--- public/products.php
+++ public/products.php
@@ -9,20 +9,24 @@
         <?php readfile("nav.html"); ?>
         <h2>Products:</h2>
         <p class="disclaimer">Disclaimer: All purchases are final, no refunds under any circumstances.</p>
-        <?php  
-            $products = scandir('products');
-            foreach($products as $prod){
-                if ($prod !== "." && $prod !== ".."){
-                    echo "<hr><h3 class=\"prod-name\">$prod</h3>";
-                    echo "<img src=\"products/$prod/main.png\" alt=\"$prod image\" class=\"prod-pic\">";
-                    echo "<p class=\"prod-desc\">";
-                    readfile("products/$prod/desc.txt");
-                    echo "</p>";
-                    echo "<p class=\"prod-price\">";
-                    readfile("products/$prod/price.txt");  
-                    echo "</p>";
+        <form action="product_page.php" method="post">
+            <?php  
+                $products = scandir('products');
+                foreach($products as $prod){
+                    if ($prod !== "." && $prod !== ".."){
+                        echo "<hr><button name=\"$prod\"";
+                        echo "<h3 class=\"prod-name\">$prod</h3>";
+                        echo "<img src=\"products/$prod/main.png\" alt=\"$prod image\" class=\"prod-pic\">";
+                        echo "</button>";
+                        echo "<p class=\"prod-desc\">";
+                        readfile("products/$prod/desc.txt");
+                        echo "</p>";
+                        echo "<p class=\"prod-price\">";
+                        readfile("products/$prod/price.txt");  
+                        echo "</p>";
+                    }
                 }
-            }
-        ?>
+            ?>
+        </form>
     </body>
 </html>
