5e6b2dd9f84541b1004cdbba1f7c78c3a55ba76e
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-10-25 22:41:38 -0700

    fixed typo

--- public/product_page.php
+++ public/product_page.php
@@ -18,7 +18,7 @@ if (isset($_COOKIE["recent"])){
     if (count($recents) > 4) unset($recents[4]);
     $ret = "$prod," . implode(",", $recents);
 
-    setcookie($prod, $recents, time() + (86400 * 30));
+    setcookie("recent", $ret, time() + (86400 * 30));
 }
 else setcookie("recent", $prod, time() + (86400 * 30));
 
