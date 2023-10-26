90467644fdde59175ffa2db0452ac28b3054f8cd
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-10-25 23:38:43 -0700

    added link to most viewed

--- public/products.php
+++ public/products.php
@@ -12,6 +12,7 @@
         <h2>Products:</h2>
         <p class="disclaimer">Disclaimer: All purchases are final, no refunds under any circumstances.</p>
         <a class="recently-viewed" href="previous_products.php">Recently viewed</a>
+        <a class="most-viewed" href="most_viewed_products.php">Most viewed</a>
 
         <?php 
             $products = scandir('products');
