278eba98f782582897cdf9ea57f69279c059f865
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-11-03 03:45:25 -0700

    changed products to use headphp

--- public/products.php
+++ public/products.php
@@ -1,20 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Products - StuffCo</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="styles.css">
-    </head>
-    <body>
-        <?php readfile("nav.html"); ?>
-        <h2>Products:</h2>
-        <p class="disclaimer">Disclaimer: All purchases are final, no refunds under any circumstances.</p>
-        <a class="recently-viewed" href="previous_products.php">Recently viewed</a>
-        <a class="most-viewed" href="most_viewed_products.php">Most viewed</a>
+<?php
+    $title = "Products";
+    include("src/element/head.php");
+?>
+<h2>Products:</h2>
+<p class="disclaimer">Disclaimer: All purchases are final, no refunds under any circumstances.</p>
+<a class="recently-viewed" href="previous_products.php">Recently viewed</a>
+<a class="most-viewed" href="most_viewed_products.php">Most viewed</a>
 
-        <?php 
-            $products = scandir('products');
-            include("src/element/product/clickable_products.php");
-        ?>
-    </body>
-</html>
+<?php 
+    $products = scandir('products');
+    include("src/element/product/clickable_products.php");
+?>
