ac751b1eebed5c4542ca538915ac84d7be351506
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-11-03 03:35:37 -0700

    fixed typo

--- public/src/element/product/full_prod.php
+++ public/src/element/product/full_prod.php
@@ -1,8 +1,8 @@
 <h3 class="prod-name"><?=$prod?></h3>
 <img src="products/<?=$prod?>/main.png" alt="<?=$prod?> image" class="prod-pic">
 <p class="prod-desc">
-    <?php readfile($_SERVER['DOCUMENT_ROOT']."products/$prod/desc.txt"); ?>
+    <?php readfile($_SERVER['DOCUMENT_ROOT']."/products/$prod/desc.txt"); ?>
 </p>
 <p class="prod-price">
-    <?php readfile($_SERVER['DOCUMENT_ROOT']."products/$prod/price.txt"); ?>
+    <?php readfile($_SERVER['DOCUMENT_ROOT']."/products/$prod/price.txt"); ?>
 </p>
