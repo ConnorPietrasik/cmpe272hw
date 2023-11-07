cc1b8688dbbdfb62f086c3890e814b6d71700e9f
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-11-06 19:12:58 -0800

    changed format

--- public/combined_users.php
+++ public/combined_users.php
@@ -12,10 +12,10 @@
     $urls = ['https://sayali-cmpe272.bayaskarpowerpack.co.in/exposed_userdb.php', 'https://subramanyajagadeesh-0a2895b9a580.herokuapp.com/exposed_users.php', 'https://rnrnattojicmpe272.000webhostapp.com/assignment-4/exposecontacts.php'];
 
     $url = 'https://cmpe272hw.pietrasik.top/user_list.php';
+    echo "<h2>".$url."</h2>";
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $users = curl_exec($ch);
-    var_dump($users);
     $data = json_decode($users);
     include("src/element/table_display.php");
     curl_close($ch);
