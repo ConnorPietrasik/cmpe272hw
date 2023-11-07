d4ceab0391313e2f85f237abe7dc5fe84fa9a3fa
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-11-06 19:10:45 -0800

    curl test

--- public/combined_users.php
+++ public/combined_users.php
@@ -11,7 +11,7 @@
 
     $urls = ['https://sayali-cmpe272.bayaskarpowerpack.co.in/exposed_userdb.php', 'https://subramanyajagadeesh-0a2895b9a580.herokuapp.com/exposed_users.php', 'https://rnrnattojicmpe272.000webhostapp.com/assignment-4/exposecontacts.php'];
 
-    $url = $urls[0];
+    $url = 'https://cmpe272hw.pietrasik.top/user_list.php';
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $users = curl_exec($ch);
