100f76e4782cb69cad465c0454ebe0b7185448af
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-11-07 02:43:56 -0800

    changed style

--- public/user_search.php
+++ public/user_search.php
@@ -2,25 +2,33 @@
     $title = "User Search";
     include("src/element/head.php");
 ?>
-<h2>User Search</h2>
-<a href="add_user.php">Add a new user</a>
-<a href="combined_users.php" style="padding-left: 5em;">All users, including other sites</a>
-<br>
-<form action="user_search.php" method="get" class="user-search">
-    <label for="first_name">First Name:</label>
-    <input type="text" id="first_name" name="first_name" value="<?= $_GET["first_name"] ?? '' ?>">
-    <br>
-    <label for="last_name">Last Name:</label>
-    <input type="text" id="last_name" name="last_name" value="<?= $_GET["last_name"] ?? '' ?>">
-    <br>
-    <label for="email">Email:</label>
-    <input type="text" id="email" name="email" value="<?= $_GET["email"] ?? '' ?>">
-    <br>
-    <label for="phone">Phone:</label>
-    <input type="text" id="phone" name="phone" value="<?= $_GET["phone"] ?? '' ?>">
-    <br>
-    <input type="submit" value="Search">
-</form>
+<table>
+    <tr>
+        <td class="column">
+            <h2>User Search</h2>
+            <form action="user_search.php" method="get" class="user-search">
+                <label for="first_name">First Name:</label>
+                <input type="text" id="first_name" name="first_name" value="<?= $_GET["first_name"] ?? '' ?>">
+                <br>
+                <label for="last_name">Last Name:</label>
+                <input type="text" id="last_name" name="last_name" value="<?= $_GET["last_name"] ?? '' ?>">
+                <br>
+                <label for="email">Email:</label>
+                <input type="text" id="email" name="email" value="<?= $_GET["email"] ?? '' ?>">
+                <br>
+                <label for="phone">Phone:</label>
+                <input type="text" id="phone" name="phone" value="<?= $_GET["phone"] ?? '' ?>">
+                <br>
+                <input type="submit" value="Search">
+            </form>
+        </td>
+        <td class="column">
+            <a href="combined_users.php">All users, including other sites</a>
+            <br>
+            <a href="add_user.php">Add a new user</a>
+        </td>
+    </tr>
+</table>
 <p class="note">Note: An empty search shows all users because it's not filtering out any</p>
 <br>
 <?php
