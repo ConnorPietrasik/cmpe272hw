acd2291847845c7096dd3a7fdb40701395415a24
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-11-06 21:40:51 -0800

    changed user_id to id

--- public/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $url = 'https://subramanyajagadeesh-0a2895b9a580.herokuapp.com/exposed_users.php';
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $users = curl_exec($ch);
-    var_dump($users);
-    curl_close($ch);
-?>
