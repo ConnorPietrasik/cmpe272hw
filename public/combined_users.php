3eaac707e934d77caeb200b6a097bfea409d88af
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-11-06 19:17:39 -0800

    added other users

--- public/combined_users.php
+++ public/combined_users.php
@@ -2,21 +2,24 @@
     $title = "Combined Users";
     include("src/element/head.php");
 ?>
-<h2>Local:</h2>
+<h2>StuffCo Users:</h2>
 <?php
     require_once("src/database.php");
     $data = $db->getAllUsers();
     
     include("src/element/table_display.php");
 
-    $urls = ['https://sayali-cmpe272.bayaskarpowerpack.co.in/exposed_userdb.php', 'https://subramanyajagadeesh-0a2895b9a580.herokuapp.com/exposed_users.php', 'https://rnrnattojicmpe272.000webhostapp.com/assignment-4/exposecontacts.php'];
+    $urls = ['Blend Station'=>'https://sayali-cmpe272.bayaskarpowerpack.co.in/exposed_userdb.php', 
+            'Student Furniture Spot'=>'https://subramanyajagadeesh-0a2895b9a580.herokuapp.com/exposed_users.php', 
+            'RNR Elektronics'=>'https://rnrnattojicmpe272.000webhostapp.com/assignment-4/exposecontacts.php'];
 
-    $url = 'https://cmpe272hw.pietrasik.top/user_list.php';
-    echo "<h2>".$url."</h2>";
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $users = curl_exec($ch);
-    $data = json_decode($users);
-    include("src/element/table_display.php");
-    curl_close($ch);
+    foreach($urls as $name => $url){
+        echo "<h3>".$name."</h3>";
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $users = curl_exec($ch);
+        $data = json_decode($users);
+        include("src/element/table_display.php");
+        curl_close($ch);
+    }
 ?>
