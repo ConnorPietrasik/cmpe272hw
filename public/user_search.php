c3ea970deb976edc261c1b8fb4e0785b0d020ec2
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-11-06 22:12:47 -0800

    switched to get

--- public/user_search.php
+++ public/user_search.php
@@ -3,18 +3,18 @@
     include("src/element/head.php");
 ?>
 <h2>User Search:</h2>
-<form action="user_search.php" method="post" class="user-search">
+<form action="user_search.php" method="get" class="user-search">
     <label for="first_name">First Name:</label>
-    <input type="text" id="first_name" name="first_name" value="<?= $_POST["first_name"] ?? '' ?>">
+    <input type="text" id="first_name" name="first_name" value="<?= $_GET["first_name"] ?? '' ?>">
     <br>
     <label for="last_name">Last Name:</label>
-    <input type="text" id="last_name" name="last_name" value="<?= $_POST["last_name"] ?? '' ?>">
+    <input type="text" id="last_name" name="last_name" value="<?= $_GET["last_name"] ?? '' ?>">
     <br>
     <label for="email">Email:</label>
-    <input type="text" id="email" name="email" value="<?= $_POST["email"] ?? '' ?>">
+    <input type="text" id="email" name="email" value="<?= $_GET["email"] ?? '' ?>">
     <br>
     <label for="phone">Phone:</label>
-    <input type="text" id="phone" name="phone" value="<?= $_POST["phone"] ?? '' ?>">
+    <input type="text" id="phone" name="phone" value="<?= $_GET["phone"] ?? '' ?>">
     <br>
     <input type="submit" value="Search">
 </form>
@@ -24,5 +24,5 @@
     $data = $db->getAllUsers();
     include("src/element/table_display.php");
 
-    var_dump($_POST);
+    var_dump($_GET);
 ?>
