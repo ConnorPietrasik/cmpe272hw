e5e67a8e7d42276fef1d6d95c8233e84eeaaae1f
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-11-06 22:06:48 -0800

    added search form

---
+++ public/user_search.php
@@ -0,0 +1,29 @@
+<?php
+    $title = "User Search";
+    include("src/element/head.php");
+?>
+<h2>User Search:</h2>
+<form action="user_search.php" method="post" class="user-search">
+    <label for="first_name">First Name:</label>
+    <input type="text" id="first_name" name="first_name" value="<?= $_POST["first_name"] ?? '' ?>">
+    <br>
+    <label for="last_name">Last Name:</label>
+    <input type="text" id="last_name" name="last_name" value="<?= $_POST["last_name"] ?? '' ?>">
+    <br>
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email" value="<?= $_POST["email"] ?? '' ?>">
+    <br>
+    <label for="phone">Phone:</label>
+    <input type="text" id="phone" name="phone" value="<?= $_POST["phone"] ?? '' ?>">
+    <br>
+    <input type="submit" value="Search">
+</form>
+<?php
+    require_once("src/database.php");
+
+    $data = $db->getAllUsers();
+    include("src/element/table_display.php");
+
+    $data = $_POST;
+    include("src/element/table_display.php");
+?>
