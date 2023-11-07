3472d710089c339e3bb6f118ba6e871679093548
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-11-06 19:09:18 -0800

    added external user list get

--- public/combined_users.php
+++ public/combined_users.php
@@ -8,4 +8,14 @@
     $data = $db->getAllUsers();
     
     include("src/element/table_display.php");
+
+    $urls = ['https://sayali-cmpe272.bayaskarpowerpack.co.in/exposed_userdb.php', 'https://subramanyajagadeesh-0a2895b9a580.herokuapp.com/exposed_users.php', 'https://rnrnattojicmpe272.000webhostapp.com/assignment-4/exposecontacts.php'];
+
+    $url = $urls[0];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $users = curl_exec($ch);
+    $data = json_decode($users);
+    include("src/element/table_display.php");
+    curl_close($ch);
 ?>
