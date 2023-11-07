25a83b4d715b10f93c3abdba525762ee3a89bfe3
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-11-06 20:18:17 -0800

    testing to help friend

---
+++ public/test.php
@@ -0,0 +1,11 @@
+<?php
+    $title = "Combined Users";
+    include("src/element/head.php");
+
+    $url = 'https://subramanyajagadeesh-0a2895b9a580.herokuapp.com/exposed_users.php';
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $users = curl_exec($ch);
+    var_dump($users);
+    curl_close($ch);
+?>
