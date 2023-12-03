ac7dabba2c558a41f0f427c91b6611b8d75ad6b7
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-12-03 01:29:28 -0800

    fixed typo

--- public/test.php
+++ public/test.php
@@ -7,10 +7,10 @@
     foreach ($companies as $c){
         echo "<h3>$c</h3>";
         $data = $db->getTopProductsHitsByCompany($c);
-        include("src/element/full_products.php");
+        include("src/element/product/full_products.php");
     }
     
     echo "<h3>All</h3>";
     $data = $db->getTopProductsHitsCombined();
-    include("src/element/full_products.php");
+    include("src/element/product/full_products.php");
 ?>
