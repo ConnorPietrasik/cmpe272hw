2826ac51c136d69b185647880e8bc0cd78d5a0f8
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-12-03 02:54:56 -0800

    added notice for no ratings and proper page

--- public/top_products/embed/by_company_rating.php
+++ public/top_products/embed/by_company_rating.php
@@ -9,7 +9,8 @@
         echo "<td class=\"column\" style=\"border: 1px solid; width: ".$width."%;\">";
         echo "<h3>".$c["name"]."</h3>";
         $products = $db->getTopProductsRatingByCompany($c["id"]);
-        include($_SERVER["DOCUMENT_ROOT"]."/src/element/product/full_products.php");
+        if (count($products)) include($_SERVER["DOCUMENT_ROOT"]."/src/element/product/full_products.php");
+        else echo "<p class=\"error\">No products from ".$c["name"]." have ratings.</p>";
     }
     echo "</tr></table>";
 ?>
