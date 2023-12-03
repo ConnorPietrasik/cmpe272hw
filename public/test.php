fbe7b8d24712db33b74ffb70289b9354aeb2eb41
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-12-02 22:33:35 -0800

    fixed typo

--- public/test.php
+++ public/test.php
@@ -1,6 +1,6 @@
 <?php
     include("src/element/head.php");
-    require_once("src/database.php");
+    require_once("src/linked_database.php");
     $db->changeCompanyDomain("StuffCo", 'https://cmpe272hw.pietrasik.top/');
     echo "Done";
 ?>
