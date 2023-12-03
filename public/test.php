95f72ae7b10b6db17a04b51b72ca841af63cf27f
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-12-03 00:26:03 -0800

    added vardump

--- public/test.php
+++ public/test.php
@@ -4,10 +4,13 @@
     require_once("src/linked_database.php");
 
     echo "<h3>Company</h3>";
-    $data = $db->getCompanyInfo(3);
+    $data = [$db->getCompanyInfo(3)];
     include("src/element/table_display.php");
 
     echo "<h3>Products</h3>";
+    echo "<pre>";
     $data = $db->getProductsByCompany(3);
-    include("src/element/table_display.php");
+    var_dump($data);
+    echo "</pre>";
+    //include("src/element/table_display.php");
 ?>
