a7acc4329bca6d07f6c35f6c5d1fc83a30fdd2ca
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-12-05 18:40:28 -0800

    added subramanya head

--- public/src/element/head.php
+++ public/src/element/head.php
@@ -1,10 +1,28 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title><?=$title ?> - StuffCo</title>
+    <title><?= $title ?> - StuffCo</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/styles.css">
+    <script>
+        window.addEventListener('message', function(event) {
+            if (event?.data?.type === 'login') {
+                localStorage.setItem("userId", event?.data.user_id)
+            }
+            if (event?.data?.type === 'logout') {
+                localStorage.removeItem("userId", event?.data.user_id);
+            }
+        });
+        if (localStorage.getItem('userId')) {
+            document.cookie = "userid=${localStorage.getItem('userId')};path=/;SameSite=None;Secure;expires=Fri, 31 Dec 9999 23:59:59 GMT"
+        }
+        if (!localStorage.getItem('userId')) {
+            document.cookie = "userid=gi;path=/;SameSite=None;Secure;expires=Fri, 31 Dec 1999 23:59:59 GMT"
+        }
+    </script>
 </head>
+
 <body>
     <table style="width:100%">
         <tr colspan="5" style="align-items: center;">
