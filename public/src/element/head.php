1141230a52f1767dcae4581dbc87c87641f33311
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-12-05 22:38:00 -0800

    changed subramanya script

--- public/src/element/head.php
+++ public/src/element/head.php
@@ -7,18 +7,18 @@
     <link rel="stylesheet" href="/styles.css">
     <script>
         window.addEventListener('message', function(event) {
-            if (event?.data?.type === 'login') {
+            if(event?.data?.type === 'login'){
                 localStorage.setItem("userId", event?.data.user_id)
             }
-            if (event?.data?.type === 'logout') {
+            if(event?.data?.type === 'logout'){
                 localStorage.removeItem("userId", event?.data.user_id);
             }
         });
-        if (localStorage.getItem('userId')) {
-            document.cookie = "userid=${localStorage.getItem('userId')};path=/;SameSite=None;Secure;expires=Fri, 31 Dec 9999 23:59:59 GMT"
+        if(localStorage.getItem('userId')){
+            document.cookie = `userid=${localStorage.getItem('userId')};path=/;SameSite=None;Secure;expires=Fri, 31 Dec 9999 23:59:59 GMT`
         }
-        if (!localStorage.getItem('userId')) {
-            document.cookie = "userid=gi;path=/;SameSite=None;Secure;expires=Fri, 31 Dec 1999 23:59:59 GMT"
+        if(!localStorage.getItem('userId')){
+            document.cookie = `userid=gi;path=/;SameSite=None;Secure;expires=Fri, 31 Dec 1999 23:59:59 GMT`
         }
     </script>
 </head>
